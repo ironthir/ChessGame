a88af2b91294069c64bd397b0156f9e652f569f9
Jakub Dzimira <jakub.dzimira@oxfordcc.co.uk>
2023-03-18 14:40:02 +0100

    ChessGame: small changes

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -6,6 +6,7 @@ public class ChessBoard {
     }
     public void InitializeChessBoard(){
 
+
     }
 
 
--- src/ChessGame.java
+++ src/ChessGame.java
@@ -7,11 +7,8 @@ enum FigureType {
     King,
 }
 
-//ChessGame - mother class, takes names of the players, starts and ends the game
-//ChessBoard - draws chessboard, initializes figures, draws figures on the screen, handles input of where you want to move a figure
-//Figure - contains current position of the figure,
 public class ChessGame {
-    public static void ChessGame(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
 
     }
